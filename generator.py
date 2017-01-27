1ac5e6fbc5c71b4d268f1c70462e427bb23ced1b
Weidi <WeidiXie@users.noreply.github.com>
2017-01-27 22:30:56 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ Created on Fri Jan 27 19:16:49 2017
 @author: Weidi Xie
 
 @description:
-This is used for augmentation on the fly.
+This is used for augmentation on-the-fly.
 """
 import numpy as np
 import re
